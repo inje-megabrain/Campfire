376cd198150ee0f8d6eb6524f44ba6281fdb6e03
Hoon9901 <shonn.dev@gmail.com>
2022-03-05 23:40:07 +0900

    chore: springboot project

---
+++ backend/src/main/java/kr/megabrain/campfireserver/CampfireserverApplication.java
@@ -0,0 +1,13 @@
+package kr.megabrain.campfireserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfireserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampfireserverApplication.class, args);
+    }
+
+}
